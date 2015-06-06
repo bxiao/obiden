110c9b90a589064d7ad452fe9f76c3a1831f900c
david allender <dallende@linux60806.dc.engr.scu.edu>
2015-06-05 17:57:06 -0700

    Changed usage string

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace obiden;
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        std::cerr << "Wrong number of args." << std::endl;
+        std::cerr << "Usage: <config_file> <self_ip:port>." << std::endl;
         return 1;
     }
     
